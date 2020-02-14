6aa18cc5a4e557aa15d16111d729c06e2be707d3
changxianglin <281532612@qq.com>
2020-02-14 23:59:17 +0800

    p and d

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  devtool: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: 'main.[contentHash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[hash].[ext]",
+              outputPath: "imgs"
+            }
+          }
+      }
+    ]
+  }
+}
