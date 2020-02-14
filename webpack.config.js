f06dfe24642eb7247578e5975651b9c473d07e73
changxianglin <281532612@qq.com>
2020-02-14 20:31:46 +0800

    file-loader emodule have some change should carefule

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,21 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[hash].[ext]",
+              outputPath: "imgs"
+            }
+          }
       }
     ]
   }
