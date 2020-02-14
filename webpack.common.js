ddd0b40d90fb7f4507c0068947de446ac5787d96
changxianglin <281532612@qq.com>
2020-02-15 00:08:49 +0800

    clean-webpack change module

--- webpack.common.js
+++ webpack.common.js
@@ -4,10 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   devtool: "none",
   entry: "./src/index.js",
-  output: {
-    filename: 'main.[contentHash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [new HtmlWebpackPlugin({
     template: "./src/template.html"
   })],
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,11 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const merge = require('webpack-merge')
 
-module.exports = {
+module.exports = merge(common, {
   mode: "development",
-  devtool: "none",
-  entry: "./src/index.js",
   output: {
-    filename: 'main.[contentHash].js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: {
-            loader: "file-loader",
-            options: {
-              esModule: false,
-              name: "[name].[hash].[ext]",
-              outputPath: "imgs"
-            }
-          }
-      }
-    ]
   }
-}
+})
