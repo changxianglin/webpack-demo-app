53b73bfff65d6d54bf497c9a3dbd10b8051eabc2
changxianglin <281532612@qq.com>
2020-02-16 14:00:47 +0800

    extract css and minify html/js/css

--- webpack.common.js
+++ webpack.common.js
@@ -7,15 +7,8 @@ module.exports = {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
